9dfc821068d8300ced6008102aec62ddf592d67b
ulianim <uliana_usa@yahoo.com>
2023-09-19 13:17:11 -0400

    new code was added to the project.js file

--- portfolio_app/src/assets/projects.js
+++ portfolio_app/src/assets/projects.js
@@ -1,21 +1,46 @@
-
-
 const projects = [
     {
         "title": "Weater App",
         "image": "./images/weather_app.png",
         "deploy": "https://ulianaim.github.io/w6_weather_calendar/",
         "gitHub": "https://github.com/ulianaim/w6_weather_calendar"
-
     },
 
     {
-        "title": "Weater App",
-        "image": "./images/weather_app.png",
+        "title": "You calendar",
+        "image": "./images/calendar.png",
+        "deploy": "https://ulianaim.github.io/w5_calendar/",
+        "gitHub": "https://github.com/ulianaim/w5_calendar"
+    },
+
+    {
+        "title": "Coding Quiz",
+        "image": "./images/quiz.png",
+        "deploy": "https://ulianaim.github.io/w4_coding_quiz/",
+        "gitHub": "https://github.com/ulianaim/w4_coding_quiz"
+    },
+
+    {
+        "title": "Weather Calendar",
+        "image": "./images/weather.png",
         "deploy": "https://ulianaim.github.io/w6_weather_calendar/",
         "gitHub": "https://github.com/ulianaim/w6_weather_calendar"
+    },
 
-    }
+    {
+        "title": "Note Taker",
+        "image": "./images/note_taker.png",
+        "deploy": "https://hidden-citadel-93640-60ca4a7091ea.herokuapp.com/",
+        "gitHub": "https://github.com/ulianaim/w_11_notes_taker-"
+    },
+
+    {
+        "title": "Text Editor",
+        "image": "./images/text_editor.png",
+        "deploy": "https://shrouded-ravine-36081-11143870e028.herokuapp.com/",
+        "gitHub": "https://github.com/ulianaim/w_19_text_editor-PWA"
+    },
+
 ]
 
 export default projects;
